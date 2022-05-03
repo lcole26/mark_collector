ad58e12265b57d4eede4008f2d26dda4b66ece06
lcole26 <lcole26@uic.edu>
2022-05-02 20:43:37 -0500

    deploy_key already expired. changed to new key for the sake of demonstration

--- javascript/rapidapi_things.js
+++ javascript/rapidapi_things.js
@@ -5,5 +5,5 @@ export const list_of_keys = [
 ];
 export const x_rapidapi_host = "ekman-emotion-analysis.p.rapidapi.com";
 export const x_rapidapi_key = "dcc9ce79a6msh1587e0491ee7c19p13b5eejsnde5141957af1";
-export const deploy_key = "76ba6ea215mshd134378db8bef74p10df4bjsn6fe4deef817f";
+export const deploy_key = "548b517443msh68ab6c7bbb432b4p121da8jsndd0e6b75b965";
 export const endpoint = "https://ekman-emotion-analysis.p.rapidapi.com/ekman-emotion";
